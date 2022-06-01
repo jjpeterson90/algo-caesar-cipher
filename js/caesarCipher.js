4780604a31c2e9ee31e817b388e79e5067e981eb
Justin <jjpeterson90@hotmail.com>
2022-06-01 17:17:23 -0500

    complete

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,3 +1,25 @@
-exports.caesarCipher = function() {
+exports.caesarCipher = function(str, cipherNum) {
+    let alpha = 'abcdefghijklmnopqrstuvwxyz';
+    let codedStr = '';
+
+    str.split('').forEach((char,i) => {
+        if (alpha.includes(char.toLowerCase())) {
+            index = alpha.indexOf(char.toLowerCase());
+            index += cipherNum;
+            if (index > 26) index -= 26;
+            if (index < 0) index += 26;
+            if (isUpperCase(char)) codedStr += alpha[index].toUpperCase();
+            else codedStr += alpha[index];
+        } else {
+            codedStr += char
+        }
+    })
+
+    function isUpperCase(char) {
+        if (char != alpha[alpha.indexOf(char.toLowerCase())]) return true
+        return false
+    }
+
+    return codedStr
 
 };
